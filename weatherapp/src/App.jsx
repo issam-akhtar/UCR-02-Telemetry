7027c2bfec310778aee109a5c735f9dc419138b9
billthai12 <billthai12@gmail.com>
2025-03-10 16:37:26 -0600

    New weather app that suits the website better!

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Weather from './components/Weather'
+import Weather2 from './components/Weather2'
 
 const App = () => {
   return (
     <div className='app'>
-      <h1> <Weather/> </h1>
+      {/* <h1> <Weather/> </h1> */}
+      <h1> <Weather2/> </h1>
     </div>
   )
 }
