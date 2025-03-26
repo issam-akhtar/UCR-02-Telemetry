697e9c2a873d4358ffbaf1ef7c9caadca0e56702
billthai12 <billthai12@gmail.com>
2025-03-26 15:26:28 -0600

    Battery Segments 1-5 and Dynamically Changing Scene

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import Weather from './components/Weather'
-import Weather2 from './components/Weather2'
-import BatteryPack from './components/BatteryPack'
 import Segment1 from './components/Segment1'
 import Segment2 from './components/Segment2'
 import Segment3 from './components/Segment3'
@@ -12,9 +9,6 @@ import CellVoltageScene from './components/cellvoltagescene'
 const App = () => {
   return (
     <div className='app'>
-      {/* <h1> <Weather/> </h1> */}
-      {/* <h1> <Weather2/> </h1> */}
-      {/* <BatteryPack/> */}
       <CellVoltageScene/>
       {/* <Segment1/>
       <Segment2/>
